3ea37a0fcd526d27df6bc99f29d934fa1568ae26
David Fallmann <a11708947@univie.ac.at>
2022-02-23 19:08:21 +0100

    added basic frame behavior

--- src/PongFrame.java
+++ src/PongFrame.java
@@ -5,9 +5,10 @@ public class PongFrame extends JFrame {
     PongPanel panel;
 
     PongFrame(){
-        new PongPanel();
-        this.setSize(500, 500);
-
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(new PongPanel());
+        this.pack();
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 }
--- src/PongPanel.java
+++ src/PongPanel.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PongPanel extends JPanel {
 
     public PongPanel() {
-
+        this.setPreferredSize(new Dimension(500, 500));
+        this.setBackground(Color.BLACK);
     }
 }
